0d9fb728e15d78007e9136b79a9a0af505f7eaf3
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-25 19:27:02 -0600

    first commit

---
+++ Ex03/index.js
@@ -0,0 +1,6 @@
+const Logger = require('logplease');
+const logger = Logger.create('utils');
+logger.debug('Hello Node.js');
+logger.info('Node.js is great!');
+logger.warn('Warning, warning, I think we have a warning');
+logger.error('Mayday maday we have an error... repeat we have an error');
