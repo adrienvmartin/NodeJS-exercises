fb0107793be04a0c40a4dd9665fe43bdcfc1fdd8
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-25 19:32:01 -0600

    Updated exercise 4

--- Ex04/index.js
+++ Ex04/index.js
@@ -1,4 +1,5 @@
 let oneLinerJoke = require('one-liner-joke');
 let getRandomJoke = oneLinerJoke.getRandomJoke();
 
-console.log(getRandomJoke);
+console.log(`Joke: ` + getRandomJoke.body);
+console.log(`Category: ` + getRandomJoke.tags);
