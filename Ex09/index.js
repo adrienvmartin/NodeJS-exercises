dabf8ad1d8721b17adc7bec22060998b19e67c80
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-25 20:21:14 -0600

    Update

--- Ex09/index.js
+++ Ex09/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// configure the default route and send a text as response
 app.get('/', function(request, response) {
   response.send("Congrats you're using your first Node.js and Express as Web Server");
 });
 
-// configure the port that express is going to listen to
 app.listen(port, function() {
   console.log(`The HTTP server is running on port ${port}`);
 });
