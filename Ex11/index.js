f60f8ecb531d7fb4d24857c3534cb35c4d5ce8b8
Adrien Martin <adrien.v.martin@gmail.com>
2018-07-27 19:26:54 -0600

    Finished exercise 11

---
+++ Ex11/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+  });
+  
+  app.listen(port, function() {
+    console.log(`The HTTP server is running on port ${port}`);
+  });
+
+app.get('/products', function(request, response) {
+    response.sendFile(path.join(__dirname, 'products.html'));
+})
