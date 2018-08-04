15b9faf1441729ffb7bfcc7d421d83768f7171a4
Adrien Martin <adrien.v.martin@gmail.com>
2018-08-03 19:46:53 -0600

    Working on Pug conditionals

--- pug2/index.js
+++ pug2/index.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about', {title: 'About', message: 'About This Page'});
+});
+
 app.listen(3000);
